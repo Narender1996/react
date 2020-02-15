e8737fc38feab83ab38112c3c8f5bfe37b3e4057
narender bisht <narender007bisht@gmail.com>
2020-02-14 21:37:57 -0800

    changes

---
+++ src/greet.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function greet()
+{
+    return <h1>hello react user</h1>
+}
+
+export default greet;
---
+++ src/view.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function hello(){
+ return(
+        <div>
+        <button>click me</button>
+            <h1>this is react headline</h1>
+            <p>hey dev ed how r u ??</p>
+            
+        </div>
+    )
+   
+}
+export default hello;
